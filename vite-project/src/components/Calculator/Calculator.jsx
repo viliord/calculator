f2a6e75f33d159a173e9f9b1c0a7a41eb97199ec
venturaviliord <venturaviliord@gmail.com>
2023-12-26 02:24:03 +0300

    calculator positioning

--- vite-project/src/components/Calculator/Calculator.jsx
+++ vite-project/src/components/Calculator/Calculator.jsx
@@ -1,13 +1,20 @@
 import styles from './styles.module.css'
 
-export const Calculator = ()=>{
+export const Calculator = () => {
 
-    return <div className={styles.container}>
-        <div className={styles.btnNumber}>
-            {[1,2,3,4,5,6,7,8,9,0].map((item)=> <button key={item}>{item}</button> )}
+    return <header>
+        <div>
+            <input type="text" id={styles.displayBoard} />
         </div>
-        <div className={styles.btnsymbols}>
-            {["+","-","*","/","="].map(item=><button key={item}>{item}</button> )}
+
+        <div className={styles.container}>
+
+            <div className={styles.btnNumber}>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((item) => <button key={item}>{item}</button>)}
+            </div>
+            <div className={styles.btnsymbols}>
+                {["+", "-", "*", "/", "="].map(item => <button key={item}>{item}</button>)}
+            </div>
         </div>
-    </div>
+    </header>
 }
